715e3819369b0dc5fd020c0fe529e0cc698804d4
Kenan Collaco <kenancollaco7@gmail.com>
2020-09-04 18:43:03 +0530

    production heroku

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+console.log(publicDirectoryPath)
+
+app.use(express.static(publicDirectoryPath))
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
