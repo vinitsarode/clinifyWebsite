0a13d35743c483e0b200b03d0b7bb5b40f5d8cf1
Kenan Collaco <kenancollaco7@gmail.com>
2020-09-30 15:12:04 +0530

    added backend dependencies

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const port = process.env.PORT || 5500
+const port = process.env.PORT
 
 const publicDirectoryPath = path.join(__dirname, '../public')
-console.log(publicDirectoryPath)
 
 app.use(express.static(publicDirectoryPath))
 
