9e7dff044b6bef536a256ea955c41ecf222c45d4
Kenan Collaco <kenancollaco7@gmail.com>
2020-09-30 16:56:12 +0530

    jobsquare api

---
+++ src/api/jobsquare.js
@@ -0,0 +1,42 @@
+var axios = require('axios');
+const { response } = require('express');
+
+const getJob = async () => {
+    const response = await axios.post('https://bsm8fqj8ug.execute-api.ap-south-1.amazonaws.com/v3/job/external-search', {
+        "partnerId": "CLINIFY",
+        "pagination": {
+            "pageNumber": 1,
+            "pageSize": 10
+        },
+        "filters": [
+            {
+                "field": "title",
+                "value": "{software developer}"
+            },
+            {
+                "field": "jobType",
+                "values": ["Full Time"] //["Full Time / Part Time / Work From Home / Internship"]
+            },
+            {
+                "field": "company",
+                "value": "{WebBee Global}" //{keyword}
+            },
+            {
+                "field": "location",
+                "Value": "{bangalore}" //{area / city / state}
+            }
+        ]
+    })
+    return response.data
+}
+
+getJob().then((data) => {
+    console.log(data)
+}).catch((e) => {
+    console.log(e)
+})
+
+module.exports = getJob
+
+
+
