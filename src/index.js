fb91bd991b13d931e132965a50c66af684eed408
vinitsarode <vinit.sarode12@gmail.com>
2020-09-30 13:09:29 +0530

    incomplete changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5500
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 console.log(publicDirectoryPath)
